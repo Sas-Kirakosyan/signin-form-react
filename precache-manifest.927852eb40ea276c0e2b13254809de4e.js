e493dc959933c8107e1dfa03411cecdff630be96
sas <sasun-kirakosyan@mail.ru>
2019-11-10 23:47:21 +0400

    Updates

---
+++ precache-manifest.927852eb40ea276c0e2b13254809de4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e21aefe4c4c15b5c10b0b1cc1cf699",
+    "url": "/Sas-Kirakosyan/signin-form-react/index.html"
+  },
+  {
+    "revision": "3aa1486362438b2cd159",
+    "url": "/Sas-Kirakosyan/signin-form-react/static/css/main.6604c1a8.chunk.css"
+  },
+  {
+    "revision": "39d899a57e2f9302306a",
+    "url": "/Sas-Kirakosyan/signin-form-react/static/js/2.18978b74.chunk.js"
+  },
+  {
+    "revision": "3aa1486362438b2cd159",
+    "url": "/Sas-Kirakosyan/signin-form-react/static/js/main.e80984df.chunk.js"
+  },
+  {
+    "revision": "4bd1a4dcad80b1d16e66",
+    "url": "/Sas-Kirakosyan/signin-form-react/static/js/runtime-main.56bb1f12.js"
+  }
+]);
